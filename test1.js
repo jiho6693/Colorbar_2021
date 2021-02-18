e0eb2f97221a20f7357685c6a33bb864909b9c2e
jiho6693 <56946702+jiho6693@users.noreply.github.com>
2021-02-18 22:06:20 +0900

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('#container');
 const SQUARE_NUM = 50;
-const colors = ['#c6045a', '#35a052', '#73965e', '#494b4a', '#a6a6a6', '#7d7a73', '#efeb5b', '#000000', '#fffcfb'];
+const colors = ['#c6045a', '#35a052', '#73965e', '#494b4a', '#a6a6a6', '#7d7a73', '#efeb5b', '#000000', '##FFFF00'];
 
 for(let i=0; i<SQUARE_NUM; i++) {
   const square = document.createElement('div');
